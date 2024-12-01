34c79558fb6827cd377882d859c2cb72a06a2cec
AldeR <koalder@yandex.ru>
2024-12-01 22:59:24 +0300

    Добавил коммент

--- marniish/siteapp/models.py
+++ marniish/siteapp/models.py
@@ -8,7 +8,7 @@ class Trend(models.Model):# Основные направления деятел
 class Article(models.Model): # Статьи
     name = models.CharField(max_length=500, unique=True)  # Библиоинфа из не более 500 символов (обычно их до 300)
     year = models.IntegerField() # Год
-    trend = models.ForeignKey(Trend, on_delete=models.CASCADE) # Направление деятельности
+    trend = models.ForeignKey(Trend, on_delete=models.CASCADE) # К какому основному направлению относится
     doi = models.CharField(max_length=50, blank=True)  # Значение DOI не выше 50 символов (обычно их до 40)
     link = models.CharField(max_length=100, blank=True)  # Ссылка на статью
     def __str__(self):
@@ -17,7 +17,7 @@ class Article(models.Model): # Статьи
 class Progress(models.Model): # Наиболее значимые достижения по направлениям НИР
     name = models.CharField(max_length=250) # Название достижения
     year = models.IntegerField() # Год
-    trend = models.ForeignKey(Trend, on_delete=models.CASCADE)  # Направление деятельности
+    trend = models.ForeignKey(Trend, on_delete=models.CASCADE)  # К какому основному направлению относится
     def __str__(self):
         return self.name # Возвращает наименование достижения
 
