6233ff9a0737fa8856dfeadbf32e1af9e094f162
AldeR <koalder@yandex.ru>
2024-12-01 23:40:59 +0300

    Добавил отображение имени записи в таблице БД TrendItem

--- marniish/siteapp/models.py
+++ marniish/siteapp/models.py
@@ -33,3 +33,5 @@ class Page(models.Model): # Страница сайта
 class TrendItem(models.Model): # Пункты направления
     name = models.CharField(max_length=250, unique=True)  # Название пункта направления
     trend = models.ForeignKey(Trend, on_delete=models.CASCADE) # К какому основному направлению относится
+    def __str__(self):
+        return self.name # Возвращает название направления
