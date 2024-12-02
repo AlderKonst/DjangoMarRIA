e33edbbf4e2325fd38c6915af5ccf32ad50f0d27
Я <koalder@yandex.ru>
2024-12-02 13:29:22 +0300

    Добавил для миграции модели таблицы БД HistoryData и History

--- marniish/siteapp/admin.py
+++ marniish/siteapp/admin.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
-from .models import * # Все модели загружаем (Trend, Article, Progress, Page, TrendItem, Reference)
+from .models import * # Все модели загружаем (Trend, Article, Progress, Page, TrendItem, Reference, HistoryData, History)
 
-admin.site.register(Trend) # Регистрируем модель Trend
-admin.site.register(Article) # Регистрируем модель Article
-admin.site.register(Progress) # Регистрируем модель Progress
-admin.site.register(Page) # Регистрируем модель Page
-admin.site.register(TrendItem) # Регистрируем модель TrendItem
-admin.site.register(Reference) # Регистрируем модель Reference
+admin.site.register(Trend) # Регистрируем модель с основными направлениями
+admin.site.register(Article) # Регистрируем модель со статьями
+admin.site.register(Progress) # Регистрируем модель с достижениями
+admin.site.register(Page) # Регистрируем модель для шаблонов страниц
+admin.site.register(TrendItem) # Регистрируем модель с пунктами направлений деятельности
+admin.site.register(Reference) # Регистрируем модель с полезными ссылками
+admin.site.register(HistoryData) # Регистрируем модель с историческими датами
+admin.site.register(History) # Регистрируем модель с историческими событиями к датам
