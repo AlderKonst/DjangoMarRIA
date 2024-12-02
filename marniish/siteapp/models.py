ffdfa1901f9e51324e2e74f2b8d0b0b167269af5
Я <koalder@yandex.ru>
2024-12-02 13:28:35 +0300

    Добавил модели таблицы БД HistoryData и History

--- marniish/siteapp/models.py
+++ marniish/siteapp/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class Trend(models.Model):# Основные направления деятельности института
     name = models.CharField(max_length=10, unique=True) # Название направления (достаточно было и 5)
     def __str__(self):
-        return self.name # Возвращает название направления
+        return self.name # Для отображения названия направления
 
 class Article(models.Model): # Статьи
     name = models.CharField(max_length=500, unique=True)  # Библиоинфа из не более 500 символов (обычно их до 300)
@@ -12,14 +12,14 @@ class Article(models.Model): # Статьи
     doi = models.CharField(max_length=50, blank=True)  # Значение DOI не выше 50 символов (обычно их до 40)
     link = models.CharField(max_length=100, blank=True)  # Ссылка на статью
     def __str__(self):
-        return self.name # Возвращает библиоинфу
+        return self.name # Для отображения библиоинфы
 
 class Progress(models.Model): # Наиболее значимые достижения по направлениям НИР
     name = models.CharField(max_length=250) # Название достижения
     year = models.IntegerField() # Год
     trend = models.ForeignKey(Trend, on_delete=models.CASCADE)  # К какому основному направлению относится
     def __str__(self):
-        return self.name # Возвращает наименование достижения
+        return self.name # Для отображения наименования достижения
 
 class Page(models.Model): # Страница сайта
     title = models.CharField(max_length=100)  # Название страницы
@@ -28,13 +28,13 @@ class Page(models.Model): # Страница сайта
     parent_url = models.URLField(max_length=30)  # URL родительской страницы (без .html)
     parent_title = models.CharField(max_length=100)  # Название родительской страницы
     def __str__(self):
-        return self.title # Возвращает название страницы
+        return self.title # Для отображения названия страницы
 
 class TrendItem(models.Model): # Пункты направления
     name = models.CharField(max_length=250, unique=True)  # Название пункта направления
     trend = models.ForeignKey(Trend, on_delete=models.CASCADE) # К какому основному направлению относится
     def __str__(self):
-        return self.name # Возвращает название направления
+        return self.name # Для отображения названия направления
 
 class Reference(models.Model): # Полезные ссылки
     name = models.CharField(max_length=100, unique=True)  # Наименование ссылки
@@ -43,4 +43,18 @@ class Reference(models.Model): # Полезные ссылки
     top = models.CharField(max_length=10, blank=True, null=True) # Расстояние до верхнего уровня в CSS
     left = models.CharField(max_length=10, blank=True, null=True) # Расстояние до левого края в CSS
     def __str__(self):
-        return self.name # Возвращает наименование ссылки
+        return self.name # Для отображения наименования ссылки
+
+class HistoryData(models.Model): # Историческая дата НИИ
+    year = models.IntegerField() # Год
+    day_month = models.CharField(max_length=15, blank=True, null=True) # Месяц и день
+    def __str__(self):
+        return self.year # Для отображения года
+
+class History(models.Model): # Исторические события НИИ
+    text = models.CharField(max_length=350, unique=True) # Наименование ссылки
+    data = models.ForeignKey(HistoryData, on_delete=models.CASCADE) # Дата события (один-ко-многим)
+    img = models.URLField(max_length=150, blank=True, null=True) # URL картинки
+    alt = models.CharField(max_length=100, blank=True, null=True) # Описание картинки
+    def __str__(self):
+        return self.text # Для отображения наименования ссылки
