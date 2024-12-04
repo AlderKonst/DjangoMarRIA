3f8169a5ebad46ae54ead00d5bee6ee334ea4ae1
Я <koalder@yandex.ru>
2024-12-04 10:53:59 +0300

    Директорию в моём ПК отдельно создал

--- marniish/siteapp/management/commands/__init__.py
+++ marniish/siteapp/management/commands/__init__.py
@@ -1,1 +1,2 @@
-site_dir = 'F:\\UII\\Python+\\DjangoMarRIA\\marniish\\templates\\MarRIA\\' # Директория страниц для парсинга
+pc_dir = 'F:\\UII\\Python+\\' # Директория в моём ПК
+site_dir = f'{pc_dir}DjangoMarRIA\\marniish\\templates\\MarRIA\\' # Директория страниц для парсинга
