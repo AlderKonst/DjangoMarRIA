91985d8279b390fb93d4255710f52cec002ca301
Я <koalder@yandex.ru>
2024-12-04 12:47:50 +0300

    Добавил функцию для преобразования персенных дат в требуемых для сохраения в БД

--- marniish/siteapp/management/commands/__init__.py
+++ marniish/siteapp/management/commands/__init__.py
@@ -1,2 +1,27 @@
-pc_dir = 'F:\\UII\\Python+\\' # Директория в моём ПК
+pc_dir = 'F:\\ДОКИ\\Python+\\' # Директория в моём ПК
 site_dir = f'{pc_dir}DjangoMarRIA\\marniish\\templates\\MarRIA\\' # Директория страниц для парсинга
+
+from datetime import datetime # Для работы со временем
+def date_transform( # Для прреобразования дат
+        date_lst, # Список отпарсенных дат
+        date_num, # Порядковых номер даты в списке date
+        date_format="%Y-%m-%d", # Формат выходного преобразования дат
+        ):
+    months = { # Словарь для преобразования названий месяцев
+        'янв': 1,
+        'фев': 2,
+        'мар': 3,
+        'апр': 4,
+        'май': 5,
+        'июн': 6,
+        'июл': 7,
+        'авг': 8,
+        'сен': 9,
+        'окт': 10,
+        'ноя': 11,
+        'дек': 12
+    }
+    day, month_str, year = date_lst[date_num].split() # Дата публикации документа с номером date_num и разделяем на составляющие
+    month = months[month_str] # Преобразуем месяц в число
+    data_obj = datetime(year=int(year), month=month, day=int(day)) # Создаём объект datetime
+    return data_obj.strftime(date_format) # Преобразуем дату и возвращаем её
