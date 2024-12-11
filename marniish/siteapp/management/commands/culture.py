0a092d2465fe0227cb302e1999cfadfc93334921
Я <koalder@yandex.ru>
2024-12-11 11:15:56 +0300

    Теперь с использованием os.path.join(site_dir, ... '

--- marniish/siteapp/management/commands/culture.py
+++ marniish/siteapp/management/commands/culture.py
@@ -1,3 +1,4 @@
+import os # Для работы с OC
 from bs4 import BeautifulSoup # Импорт библиотеки для парсинга HTML
 from django.core.files import File # Для работы с файлами в Django
 from django.core.management.base import BaseCommand # Импорт базового класса команды Django
@@ -12,7 +13,7 @@ class Command(BaseCommand):
                  'Jim': 'Плодово-ягодные культуры', 'Potato': 'Клубнеплоды'} # Словарь имён html-файлов и группы культур
         for file, group in files.items(): # Итерируем по имени файла сайта и группы агрокультур из списка
             group, _ = CultureGroup.objects.get_or_create(name=group)  # Создаем объект CultureGroup с его названием
-            with open(f'{site_dir}{file}.html', 'r', encoding='utf-8') as f: # Открываем для чтения каждый html-файл
+            with open(os.path.join(site_dir, file, '.html'), 'r', encoding='utf-8') as f: # Открываем для чтения каждый html-файл
                 content = f.read() # Читаем содержимое файла с кодом
                 soup = BeautifulSoup(content, 'html.parser') # Парсим исходный HTML-код
                 articles = soup.find_all('article')[:-1] # Извлекаем все article-теги, кроме последнего
@@ -24,14 +25,14 @@ class Command(BaseCommand):
                     pp = article.find_all('p')  # Извлекаем содержимое из каждого абзаца <p>
                     text = '\n'.join(str(p) for p in pp) # Создаем строку содержимого из всех абзацев <p> блока
                     img = article.find('img') if article.find('img') else '' # Извлекаем изображение, если есть
-                    src = img.get('src') if img else '' # Извлекаем путь к изображению, если есть
+                    src = img.get('src').src.split('/') if img else '' # Извлекаем путь к изображению, если есть
                     alt = img.get('alt') if img else '' # Извлекаем альт-текст изображения, если есть
                     if img:
-                        with open(f'{site_dir}{src}', 'rb') as img_file:
+                        with open(os.path.join(site_dir, *src), 'rb') as img_file:
                             Taxon.objects.create(name=taxon, # Создаем объект с таблицей History с текстом
                                                  culture=culture, # Добавляем объект по виду культуры
                                                  text=text, # Добавляем содержимое
-                                                 img=File(img_file, name=src), # Передаем файл изображения
+                                                 img=File(img_file, name=os.path.join(*src)), # Передаем файл изображения
                                                  alt=alt) # Добавляем альт-текст изображения
                     else:
                         Taxon.objects.create(name=taxon, # Создаем объект с таблицей History с текстом
