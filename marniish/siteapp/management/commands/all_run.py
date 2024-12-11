5d37366d3f5dea8ce4629aa383e2f9ade3d83ddb
Я <koalder@yandex.ru>
2024-12-11 10:03:04 +0300

    Обернул в class Command(BaseCommand) c def handle

--- marniish/siteapp/management/commands/all_run.py
+++ marniish/siteapp/management/commands/all_run.py
@@ -1,19 +1,26 @@
+from django.core.management.base import BaseCommand # Импорт базового класса команды Django
 import os # Загружаем модуль os
 
-def all_run(): # Создаем функцию запуска всех своих команд
-    commands = [
-        article, # Статьи
-        culture, # Культуры
-        doc, # Документы
-        history, # История
-        news, # Новости
-        page, # Страницы
-        progress, # Достижения
-        reference, # Полезные ссылки
-        trenditem # Направления деятельности
-    ]
-    for command in commands: # Перебираем все команды
-        os.system(f'python manage.py {command}') # Запускаем каждую команду
+class Command(BaseCommand):
+    help = 'Запускает все команды'
 
-if __name__ == '__main__': # Если это файл с именем __main__, то запускаем функцию all_run()
-    all_run() # Запускаем функцию all_run()
+    def handle(self, *args, **kwargs):  # Переопределяем метод handle
+        self.all_run()  # Вызываем функцию all_run()
+
+    def all_run(self):  # Создаем функцию запуска всех своих команд
+        commands = [
+            'article',  # Статьи
+            'culture',  # Культуры
+            'doc',      # Документы
+            'history',   # История
+            'news',     # Новости
+            'page',     # Страницы
+            'progress',  # Достижения
+            'reference',  # Полезные ссылки
+            'trenditem'  # Направления деятельности
+        ]
+        for command in commands:  # Перебираем все команды
+            os.system(f'python manage.py {command}')  # Запускаем каждую команду
+
+if __name__ == '__main__':  # Если это файл с именем __main__, то запускаем функцию all_run()
+    Command().all_run()  # Запускаем функцию all_run()
