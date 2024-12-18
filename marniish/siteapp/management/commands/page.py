294022dddd57e512b77b58eabb2893d65d810e08
AldeR <koalder@yandex.ru>
2024-12-18 18:55:04 +0300

    Добавил поведение, где если адрес не начинается с News (остальные), чтобы format_url(url) возвращал адрес без слеша

--- marniish/siteapp/management/commands/page.py
+++ marniish/siteapp/management/commands/page.py
@@ -28,13 +28,15 @@ class Command(BaseCommand):
                         if url.startswith('News'): # Если адрес начинается с News
                             year = url[4:] # Извлекаем год из имени страницы
                             return f"News/{year}" # Форматируем URL как News/ГГГГ
+                        else: # Если адрес не начинается с News (остальные)
+                            return url # Возвращаем адрес страницы без .html
 
                     url = format_url(page) # Убираем .html из имени текущей страницы с таким (News/ГГГГ) форматом
                     parent_url = format_url(parent_url) # Убираем .html из адреса родительской страницы с таким (News/ГГГГ) форматом
-                Page.objects.get_or_create( # Создаем объект таблицы БД Page
-                    title=title,  # Создаем поле именем страницы
-                    url = url, # Создаем поле адреса текущей страницы без расширения
-                    description = description, # Создаем поле описания страницы
-                    parent_url = parent_url, # Создаем поле адреса родительской страницы без расширения
-                    parent_title = parent_title # Создаем поле имени родительской страницы
-                )
+                    Page.objects.get_or_create( # Создаем объект таблицы БД Page
+                        title=title,  # Создаем поле именем страницы
+                        url = url, # Создаем поле адреса текущей страницы без расширения
+                        description = description, # Создаем поле описания страницы
+                        parent_url = parent_url, # Создаем поле адреса родительской страницы без расширения
+                        parent_title = parent_title # Создаем поле имени родительской страницы
+                    )
